caab6765cd3bcbe8619a17680ea77657c5da8660
= <=>
2019-10-09 19:34:59 -0300

    Teste 2 adicionar linha

--- Principal.java
+++ Principal.java
@@ -1,6 +1,7 @@
 public class Principal {
 
 	public static void main(String[] args) {
+		
 		System.out.println("Primeira edição Git");
 		System.out.println("Segunda edição Git");
 		System.out.println("Terceira edição Git");
