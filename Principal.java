3b098195024571299a055947137e937a12452696
= <=>
2019-10-09 18:56:02 -0300

    Primeiro Commit

---
+++ Principal.java
@@ -0,0 +1,10 @@
+
+public class Principal {
+
+	public static void main(String[] args){
+		System.out.println("primeira linha de comando");
+		System.out.println("segunda linha de comando");
+		System.out.println("terceira linha de comando");
+	}
+	
+}
