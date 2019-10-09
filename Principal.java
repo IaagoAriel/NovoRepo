f0161ed078f9279731baa04c5156573be44cd2f9
= <=>
2019-10-09 19:06:22 -0300

    Segundo Commit

--- Principal.java
+++ Principal.java
@@ -1,10 +1,8 @@
-
 public class Principal {
 
 	public static void main(String[] args){
-		System.out.println("primeira linha de comando");
-		System.out.println("segunda linha de comando");
-		System.out.println("terceira linha de comando");
+		System.out.println("Encaminhando alterações...");
+		
 	}
 	
 }
