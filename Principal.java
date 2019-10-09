d256516ea40704f028ddd68c383978e371de948d
= <=>
2019-10-09 19:15:54 -0300

    Exclusão da última linha de comando

--- Principal.java
+++ Principal.java
@@ -1,7 +1,9 @@
 public class Principal {
 
 	public static void main(String[] args){
-		System.out.println("Encaminhando alterações...");
+		
+		System.out.println("Primeira edição Git");
+		System.out.println("Segunda edição Git");
 		
 	}
 	
